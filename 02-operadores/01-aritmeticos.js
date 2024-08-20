b42798b2ae44843fb70bfc6c8439aeae8907955a
josemills <138175607+josemills@users.noreply.github.com>
2024-08-19 20:25:47 -0400

    Add files via upload

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+//console.log( a + b, 'adicion'); // adición
+// console.log( a - b, 'resta' ); // resta
+// console.log( a * b, 'multiplicacion'); // multiplicación
+// console.log( a / b, 'division'); // división
+// console.log( a % b, 'modulo'); // módulo
+// console.log( a ** b, 'potencia'); // potencia
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
